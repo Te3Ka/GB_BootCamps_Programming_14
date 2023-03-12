eac2b62b00a5422c262f1b2c553ecd9dfa66ff64
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-12 18:51:35 +0300

    Сделаны перемножения

--- Program.cs
+++ Program.cs
@@ -1,34 +1,79 @@
 ﻿double[,] GenerateMatrix(int rows, int columns)
 {
-    double[,] result = new double[rows, columns]
-
+    double[,] result = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = Random.Shared.Next(1, 5);
+        }
+    }
     return result;
 }
 
 void PrintMatrix(double[,] matrix)
 {
-
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 }
 
 double[,] MultiplySync(double[,] m1, double[,] m2)
 {
-    int i = 0, j = 0, k = 0;
-    double[,] result = new double[i, j];
+    int rowsM1 = m1.GetLength(0),
+        k = m1.GetLength(1),
+        columnsM2 = m2.GetLength(1);
+    double[,] result = new double[rowsM1, columnsM2];
+
+    for (int i = 0; i < rowsM1; i++)
+    {
+        for (int j = 0; j < columnsM2; j++)
+        {
+            double s = 0;
+            for (int r = 0; r < k; r++)
+            {
+                s += m1[i, r] * m2[r, j];
+            }
+            result[i, j] = s;
+        }
+    }
 
     return result;
 }
 
 double[,] MultiplyParallel(double[,] m1, double[,] m2)
 {
-    int i = 0, j = 0, k = 0;
-    double[,] result = new double[i, j];
+    int rowsM1 = m1.GetLength(0),
+        k = m1.GetLength(1),
+        columnsM2 = m2.GetLength(1);
+    double[,] result = new double[rowsM1, columnsM2];
+    
+    Parallel.For(0, rowsM1, i =>
+    {
+        for (int j = 0; j < columnsM2; j++)
+        {
+            double s = 0;
+            for (int r = 0; r < k; r++)
+            {
+                s += m1[i, r] * m2[r, j];
+            }
+            result[i, j] = s;
+        }
+    });
 
     return result;
 }
 
-var m1 = GenerateMatrix(100, 100);
+int n = 4;
+var m1 = GenerateMatrix(n, n);
 PrintMatrix(m1);
-var m2 = GenerateMatrix(100, 100);
+var m2 = GenerateMatrix(n, n);
 PrintMatrix(m2);
 var mSync = MultiplySync(m1, m2);
 PrintMatrix(mSync);
