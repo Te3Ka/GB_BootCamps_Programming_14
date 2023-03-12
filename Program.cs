b57a999a59e5b9e8d8c6d73cd307dd0f5c89885b
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-12 17:55:33 +0300

    Начало

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] GenerateMatrix(int rows, int columns)
+{
+    double[,] result = new double[rows, columns]
+
+    return result;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+
+}
+
+double[,] MultiplySync(double[,] m1, double[,] m2)
+{
+    int i = 0, j = 0, k = 0;
+    double[,] result = new double[i, j];
+
+    return result;
+}
+
+double[,] MultiplyParallel(double[,] m1, double[,] m2)
+{
+    int i = 0, j = 0, k = 0;
+    double[,] result = new double[i, j];
+
+    return result;
+}
+
+var m1 = GenerateMatrix(100, 100);
+PrintMatrix(m1);
+var m2 = GenerateMatrix(100, 100);
+PrintMatrix(m2);
+var mSync = MultiplySync(m1, m2);
+PrintMatrix(mSync);
+var mParallel = MultiplyParallel(m1, m2);
+PrintMatrix(mParallel);
